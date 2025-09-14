cdaa32ed3b20cfb5f5a8ad7ac0d6921f3bfe39a8
Akjo <lukas.kueffer@outlook.com>
2025-09-14 20:52:13 +0200

    Made the demo application show more of the current state of the engine

--- src/main/java/com/akjostudios/engine/demo/AkjoEngineDemoApp.java
+++ src/main/java/com/akjostudios/engine/demo/AkjoEngineDemoApp.java
@@ -2,11 +2,29 @@ package com.akjostudios.engine.demo;
 
 import com.akjostudios.engine.api.AkjoApplication;
 import com.akjostudios.engine.api.context.Component;
+import com.akjostudios.engine.api.event.EventHandler;
+import com.akjostudios.engine.api.scheduling.SchedulerLane;
+import com.akjostudios.engine.api.window.WindowMode;
+import com.akjostudios.engine.api.window.events.*;
+import org.jetbrains.annotations.NotNull;
 
 @Component
 public class AkjoEngineDemoApp extends AkjoApplication {
     @Override
     public void onStart() {
-        ctx.lifecycle().stopApplication();
+        ctx.scheduler().runOnceNextFrame(
+                () -> ctx.windows().builder(
+                        "Demo Application",
+                        WindowMode.WINDOWED,
+                        ctx.monitors()::getPrimaryMonitor,
+                        true
+                ).build(),
+                SchedulerLane.RENDER
+        );
+    }
+
+    @EventHandler
+    public void onAllWindowsClosed(@NotNull AllWindowsClosedEvent ignored) {
+        ctx.lifecycle().stopApplication("All windows have been closed.");
     }
 }
