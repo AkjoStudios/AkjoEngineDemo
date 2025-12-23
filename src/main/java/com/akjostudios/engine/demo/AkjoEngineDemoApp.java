1afca2ef4dcff39318ccd7e95813ade3ab7b8650
Akjo <lukas.kueffer@outlook.com>
2025-12-23 16:33:21 +0100

    Fixed window modes

--- src/main/java/com/akjostudios/engine/demo/AkjoEngineDemoApp.java
+++ src/main/java/com/akjostudios/engine/demo/AkjoEngineDemoApp.java
@@ -4,6 +4,7 @@ import com.akjostudios.engine.api.AkjoApplication;
 import com.akjostudios.engine.api.common.base.color.Color;
 import com.akjostudios.engine.api.context.Component;
 import com.akjostudios.engine.api.event.EventHandler;
+import com.akjostudios.engine.api.window.WindowMode;
 import com.akjostudios.engine.api.window.events.AllWindowsClosedEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,8 +12,8 @@ import org.jetbrains.annotations.NotNull;
 public class AkjoEngineDemoApp extends AkjoApplication {
     @Override
     public void onStart() {
-        ctx.windows().create("Demo Application", ctx.monitors()::getPrimaryMonitor)
-                .thenAccept(window -> window.onRender(() -> window.canvas().clear(Color.WHITE)));
+        ctx.windows().create("Demo Application", WindowMode.FULLSCREEN, ctx.monitors()::getPrimaryMonitor)
+                .thenAccept(window -> window.onRender(() -> window.canvas().clear(Color.TEAL)));
     }
 
     @EventHandler
